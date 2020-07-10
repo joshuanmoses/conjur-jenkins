9d6ae544031216745c26d84878de23c1fda26e88
Joshua Moses <62614287+joshuamoses-cyberark@users.noreply.github.com>
2020-07-09 20:39:23 -0500

    Update 3-export-copy-cert-toJenkins-Host.sh

--- 3-export-copy-cert-toJenkins-Host.sh
+++ 3-export-copy-cert-toJenkins-Host.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Extract conjur.pem cert from DAP Master
-docker exec -t dap1 bash -c "openssl s_client -showcerts -connect dap-master.cyber-ark-demo.local:443 < /dev/null 2> /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /tmp/conjur.pem"
+docker exec -t dap1 bash -c "openssl s_client -showcerts -connect dap-master.myorg.local:443 < /dev/null 2> /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /tmp/conjur.pem"
 
 #copy to Docker host
 docker cp dap1:/tmp/conjur.pem .
 
 #copy cert to Jenkins Host
-sudo scp conjur.pem user01@10.0.0.20:/home/user01
+sudo scp conjur.pem jmoses@10.10.10.100:/home/jmoses
